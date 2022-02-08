dc06e35fb2e5b2ed59b1fffb5a6f3108e3469945
Thomas <thomas@thomasspina.com>
2022-02-07 22:15:37 -0500

    done barebones of website

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,23 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessageBoard = document.querySelector('.chat-message-board')
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('room-users');
 
 // Get username and room from URL
-const { username, room } = qs.parse(location.search, {
-    ignoreQueryPrefix = true
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
 });
 
 const socket = io();
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
 
 // Message from server
 socket.on('message', message => {
@@ -47,4 +55,14 @@ function outputMessage(message) {
     `;
 
     document.querySelector('.chat-message-board').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `${users.map(user => `<li>${user.username}</li>`).join('')}`;
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/message.js');
-const { userJoin, getCurrentUser } = require('./utils/user.js');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/user.js');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,7 +17,7 @@ const botName = 'chat bot';
 // Run when a client connects
 io.on('connection', socket => {
     socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room); // TODO update user system
+        const user = userJoin(socket.id, username, room); // TODO update user system (like what is socket.id and maybe use custome ids)
 
         socket.join(user.room);
 
@@ -26,16 +26,34 @@ io.on('connection', socket => {
 
         // Broadcast when a user connects
         socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        // Send users room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg));
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
 
     // Broadcast when a user connects
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'USER has left the chat'));
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+            // Send users room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+        });
+        }
     });
 
 });
